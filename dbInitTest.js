13b168a99c920431b19872d0e0401a2526219919
NanduKkd <55228729+NanduKkd@users.noreply.github.com>
2023-06-12 11:36:14 +0530

    Env config and db initializer

--- dbInitTest.js
+++ dbInitTest.js
@@ -37,8 +37,8 @@ database.once('connected', async() => {
 	const superAdminPerson = new personModel({
 		joiningdate: new Date(2022, 4, 1),
 		monthsprobation: 3,
-		name: "Nanda Krishnan",
-		email: "nandukkd7164@gmail.com",
+		name: "Vijay KT",
+		email: "vijaykt@gmail.com",
 		admin: true,
 		superadmin: true,
 		post: post._id,
@@ -49,6 +49,47 @@ database.once('connected', async() => {
 	await superAdminPerson.save()
 	await LeaveManager.onEmployeeAdded(superAdminPerson)
 
+
+	// Site Engineer
+	const department2 = new departmentModel({name: "Engineering", preset: true})
+	await department2.save();
+	const siteEngineer = new postModel({name: "Senior Site Engineer"})
+	await siteEngineer.save()
+	const siteEngineerPerson = new personModel({
+		joiningdate: new Date(2022, 4, 1),
+		monthsprobation: 3,
+		name: "Akhilesh PV",
+		email: "akhilesh@gmail.com",
+		admin: false,
+		superadmin: false,
+		post: siteEngineer._id,
+		department: department2._id,
+		password: '$2b$08$6RnWj2mzMIZ2SiRMcMZBd.eDr0/aR9kkyNkNU1uSPqnlz2eAHwqiq',
+		onsite: true,
+		attendancereportingperson: new mongoose.Types.ObjectId(superAdminPerson),
+		leavereportingperson: new mongoose.Types.ObjectId(superAdminPerson),
+	})
+	await siteEngineerPerson.save()
+	await LeaveManager.onEmployeeAdded(siteEngineerPerson)
+
+	const jrSiteEngineer = new postModel({name: "Junior Site Engineer"})
+	await jrSiteEngineer.save()
+	const jrSiteEngineerPerson = new personModel({
+		joiningdate: new Date(2022, 4, 1),
+		monthsprobation: 3,
+		name: "Ajith KT",
+		email: "ajith@gmail.com",
+		admin: false,
+		superadmin: false,
+		post: jrSiteEngineer._id,
+		department: department2._id,
+		password: '$2b$08$6RnWj2mzMIZ2SiRMcMZBd.eDr0/aR9kkyNkNU1uSPqnlz2eAHwqiq',
+		onsite: true,
+		attendancereportingperson: new mongoose.Types.ObjectId(siteEngineerPerson),
+		leavereportingperson: new mongoose.Types.ObjectId(siteEngineerPerson),
+	})
+	await jrSiteEngineerPerson.save()
+	await LeaveManager.onEmployeeAdded(jrSiteEngineerPerson)
 
 	database.close()
 })
